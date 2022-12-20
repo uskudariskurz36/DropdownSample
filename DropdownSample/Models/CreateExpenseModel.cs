7fea3ddc214d901cccee673d6a9613ea38651676
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-20 09:27:01 +0300

    .

---
+++ DropdownSample/Models/CreateExpenseModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DropdownSample.Models
+{
+    public class CreateExpenseModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public DateTime? Date { get; set; }
+        public decimal Amount { get; set; }
+
+        [StringLength(50)]
+        public string? Category { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
