74697690450a40b44eeb0ffef4e96dc5241a0547
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-20 08:59:41 +0300

    Add project files.

---
+++ DropdownSample/Helpers/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DropdownSample.Helpers
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
+        public DatabaseContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=Z36DropdownSampleDB;Trusted_Connection=true");
+            }
+        }
+    }
+}
---
+++ DropdownSample/Models/ProfilePictureModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DropdownSample.Models
+{
+    public class ProfilePictureModel
+    {
+        [Required]
+        public IFormFile NewPicture { get; set; }
+    }
+}
